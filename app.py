624b4461deb0d09e58782a09344a91bd6ef25acf
antio.tseng <antio.tseng@yahoo.com.tw>
2023-06-19 11:22:15 +0800

    cancel botton & user info interface optimization

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 import sqlite3
 import datetime
+import random
 from flask_login import (
     LoginManager,
     UserMixin,
@@ -115,11 +116,7 @@ def view2():
     con = sqlite3.connect("Gamble.db")
     con.row_factory = sqlite3.Row
     cur = con.cursor()
-    date = "2023-04-18"
-    cur.execute(
-        # "SELECT * FROM match_info WHERE date > DATE('2023-04-18') AND date = (SELECT date FROM single_match WHERE date > DATE('2023-04-18') Order By date asc);"
-        "SELECT * FROM match_info WHERE `gametype` = '單場賽場' Order By date;"
-    )
+    cur.execute("SELECT * FROM match_info WHERE `賽事種類` = '單場賽場' Order By date;")
     con.commit()
     rows = cur.fetchall()
     return render_template("view2.html", rows=rows, logged_in=logged_in)
@@ -136,9 +133,9 @@ def view3():
     con = sqlite3.connect("Gamble.db")
     con.row_factory = sqlite3.Row
     cur = con.cursor()
-    cur.execute("Select * from match_info WHERE `賽gametype` = '系列賽' Order By date")
+    cur.execute("Select * from match_info WHERE `賽事種類` = '系列賽' Order By date")
     rows = cur.fetchall()
-
+    con.close()
     return render_template("view2.html", rows=rows, logged_in=logged_in)
 
 
@@ -155,7 +152,7 @@ def team():
     cur = con.cursor()
     cur.execute("Select * from NBA_playoff_teams  ")
     rows = cur.fetchall()
-
+    con.close()
     return render_template("team.html", rows=rows, logged_in=logged_in)
 
 
@@ -172,6 +169,7 @@ def player():
     cur = con.cursor()
     cur.execute("Select * from NBA_playoff_players  ")
     rows = cur.fetchall()
+    con.close()
 
     return render_template("player.html", rows=rows, logged_in=logged_in)
 
@@ -287,6 +285,7 @@ def money_add():
         (user_id, action_text, in_out_money),
     )
     con.commit()
+    con.close()
 
     return render_template(
         "money_in_out_success.html",
@@ -313,6 +312,7 @@ def money_in_out_record():
         [user_id],
     )
     records = cur.fetchall()
+    con.close()
 
     return render_template("money_in_out_record.html", records=records)
 
@@ -332,6 +332,7 @@ def gameplay():
     curr.execute("SELECT * FROM bet WHERE gametype = '單場賽事'; ")
     con.commit()
     results = curr.fetchall()
+    con.close()
 
     return render_template(
         "gameplay_view2.html", rows=rows, results=results, logged_in=logged_in
@@ -354,9 +355,11 @@ def bet1():
     cur = con.cursor()
     date = "2023-04-18"
     cur.execute(
-        "SELECT bet.matchId,match_info.home,match_info.away,bet.gametype,bet.gamble,bet.line FROM bet,match_info where gametype = '單場賽事' and match_info.matchId=bet.matchId and gametype = '單場賽事' and date > DATE('2023-04-18') and date = (SELECT date FROM match_info WHERE date > DATE('2023-04-18') Order By date asc);"
+        "SELECT bet.matchId, match_info.home, match_info.away, bet.gametype, bet.gamble, bet.line FROM bet,match_info where gametype = '單場賽事' and match_info.matchId=bet.matchId and gametype = '單場賽事' and date > DATE('2023-04-18') and date = (SELECT date FROM match_info WHERE date > DATE('2023-04-18') Order By date asc);"
     )
     rows = cur.fetchall()
+    con.close()
+
     return render_template("bet1.html", rows=rows)
 
 
@@ -370,6 +373,8 @@ def bet2():
         "SELECT bet.matchId,match_info.home,match_info.away,bet.gametype,bet.gamble,bet.line FROM bet,match_info where match_info.matchId=bet.matchId and gametype = '系列賽' and date > DATE('2023-04-15') and date = (SELECT date FROM match_info WHERE date > DATE('2023-04-15') Order By date asc);"
     )
     rows = cur.fetchall()
+    con.close()
+
     return render_template("bet2.html", rows=rows)
 
 
@@ -380,11 +385,13 @@ def bet1_in():
     con = sqlite3.connect("Gamble.db")
     con.row_factory = sqlite3.Row
     cur = con.cursor()
+
     user_id = current_user.id
     single = request.form["single"]
     project = request.form["project"]
     money = int(request.form["money"])
     password = request.form["password"]
+
     cur.execute("select money FROM member_info WHERE password = ?", (password,))
     current_money = cur.fetchone()[0]
     cur.execute(
@@ -398,10 +405,11 @@ def bet1_in():
     )
     bet_win_money = cur.fetchone()[0]
     cur.execute(
-        "INSERT INTO member_record (memberId, 賽事編號, 種類,下注金額,可獲得金額,中獎結果) VALUES (?, ?, ?,?,?,?)",
-        (user_id, single, project, money, bet_win_money, "未中獎"),
+        "INSERT INTO member_record (memberId, 賽事編號, 種類, 下注金額, 可獲得金額, 中獎結果) VALUES (?, ?, ?,?,?,?)",
+        (user_id, single, project, money, bet_win_money, "未開獎"),
     )
     con.commit()
+    con.close()
 
     return render_template(
         "bet_success.html",
@@ -424,6 +432,7 @@ def bet2_in():
     project = request.form["project"]
     money = int(request.form["money"])
     password = request.form["password"]
+
     cur.execute("select money FROM member_info WHERE password = ?", (password,))
     current_money = cur.fetchone()[0]
     cur.execute(
@@ -437,10 +446,11 @@ def bet2_in():
     )
     bet_win_money = cur.fetchone()[0]
     cur.execute(
-        "INSERT INTO member_record (memberId, 賽事編號, 種類,下注金額,可獲得金額,中獎結果) VALUES (?, ?, ?,?,?,?)",
-        (user_id, single, project, money, bet_win_money, "未中獎"),
+        "INSERT INTO member_record (memberId, 賽事編號, 種類, 下注金額, 可獲得金額, 中獎結果) VALUES (?, ?, ?, ?, ?, ?)",
+        (user_id, single, project, money, bet_win_money, "未開獎"),
     )
     con.commit()
+    con.close()
 
     return render_template(
         "bet_success.html",
@@ -455,8 +465,39 @@ def bet2_in():
 @app.route("/member_infor")
 @login_required
 def member_infor():
+    con = sqlite3.connect("Gamble.db")
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()
+
     user_id = current_user.id
-    return render_template("member_infor.html")
+
+    cur.execute("select money FROM member_info WHERE memberId = ?", (user_id,))
+    current_money = cur.fetchone()[0]
+
+    cur.execute(
+        "select count(id) FROM money_records WHERE user_id=? AND action='儲值'",
+        (user_id,),
+    )
+    in_money_time = cur.fetchone()[0]
+
+    cur.execute(
+        "select count(id) FROM money_records WHERE user_id=? AND action='提領'",
+        (user_id,),
+    )
+    out_money_time = cur.fetchone()[0]
+
+    cur.execute("select count(id) FROM member_record WHERE memberId=?", (user_id,))
+    gamble_time = cur.fetchone()[0]
+
+    con.close()
+    return render_template(
+        "member_infor.html",
+        user_id=user_id,
+        current_money=current_money,
+        in_money_time=in_money_time,
+        out_money_time=out_money_time,
+        gamble_time=gamble_time,
+    )
 
 
 # 會員資料下注紀錄查詢
@@ -467,12 +508,73 @@ def bet_record():
     con.row_factory = sqlite3.Row
     cur = con.cursor()
     user_id = current_user.id
+
+    # 設置日期
+    date = "2023-04-17"
+
     cur.execute(
-        "SELECT member_record.賽事編號, member_record.種類, member_record.下注金額, member_record.可獲得金額, member_record.中獎結果 FROM member_record where memberId=?",
+        """
+    SELECT
+        member_record.id,
+        member_record.賽事編號,
+        member_record.種類,
+        member_record.下注金額,
+        member_record.可獲得金額,
+        member_record.中獎結果,
+        match_info.date
+    FROM
+        member_record
+    LEFT JOIN
+        match_info
+    ON
+        member_record.賽事編號 = match_info.matchId
+    WHERE
+        memberId=?
+    """,
         (user_id,),
     )
+
     rows = cur.fetchall()
+
+    # 遍历查询结果并调整中獎結果
+    for row in rows:
+        bet_date = row["date"]
+        result = row["中獎結果"]
+
+        # 判断是否需要调整中獎結果
+        if bet_date <= date:
+            # 随机选择一半概率调整中獎結果
+            if random.random() < 0.5:
+                result = "已中獎"
+            else:
+                result = "未中獎"
+
+        # 更新中獎結果到数据库
+        cur.execute(
+            "UPDATE member_record SET 中獎結果=? WHERE id=?",
+            (result, row["id"]),
+        )
+    con.commit()
+    con.close()
+
     return render_template("bet_record.html", rows=rows)
+
+
+# 刪除下注紀錄
+@app.route("/delete_record", methods=["POST"])
+@login_required
+def delete_record():
+    data = request.get_json()
+    bet_id = data["betId"]
+
+    # 連接資料庫並刪除對應記錄
+    con = sqlite3.connect("Gamble.db")
+    cur = con.cursor()
+    cur.execute("DELETE FROM member_record WHERE id=?", (bet_id,))
+    con.commit()
+    con.close()
+
+    return "Record deleted successfully"
 
 
 # 操作中獎介面
@@ -502,6 +604,7 @@ def operate_success():
         ("已中獎", single, project),
     )
     con.commit()
+    con.close()
     return render_template("operate_success.html", single=single, project=project)
 
 
